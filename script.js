f29ce11c2589e101b127fd3cdca3f85f87eda1fe
TALUPULAPADMAPAVANI <234g1a33b3@srit.ac.in>
2025-09-15 12:22:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("===Hour 1:JSON Basics ===");
+
+ let student={
+    name:"Asha",
+    age:21,
+    marks:[85,90,78]
+ }
+
+ //convert object+json string
+ let jsonString=JSON.stringify(student);
+console.log("JSON string:",jsonString);
+
+//convert JSON string+object
+ let parseObj=JSON.parse(student);
+console.log("Parsed Object:",parseObj);
+
+//fetch sample JSON from API
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+   .then(response => response.json()) //convert to json
+   .then(data => console.log("Fetched Data:",data))
+   .catch(error => console.error("Error:",error));
+
+
+
+//fetch all the users and show in console+page
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(users => {
+        let output="<h3>User List</h3><ul>";
+        users.forEach(user =>{
+            output+=`<li>${user.name} - ${user.email}</li>`;
+        });
+        output +="</ul>";
+        //Append result to page
+        document.body.innerHTML += output;
+    });
